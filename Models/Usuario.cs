86b1bb78d17bcbd6714cbeeb1ba73c3059a2030c
Fernando Costa <fernando.costa@targetsoftware.com.br>
2020-12-01 18:42:04 -0300

    Instaldo os frameworks restanes

---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+namespace Teste.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string LoginName {get; set}
+    }
+}
